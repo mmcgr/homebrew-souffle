2820d444314763c117a73f2bc3c34ebb42c5cd62
mmcgr <mmcgr@users.noreply.github.com>
2019-03-07 13:02:06 +1100

    Update to use git repo

--- Formula/souffle.rb
+++ Formula/souffle.rb
@@ -1,18 +1,20 @@
 class Souffle < Formula
   desc "Translator of declarative Datalog programs into the C++ language."
   homepage "https://github.com/souffle-lang/souffle/wiki"
-  url "https://dl.bintray.com/souffle-lang/osx/souffle-1.5.1-280-gaf2daea1.pkg"
-  sha256 "048fd84e72bed78ffd54968bfd7b3fe9d02a6945d7421e82064956623d472771"
+  url "https://github.com/souffle-lang/souffle.git"
 
   depends_on "libtool" => :build
   depends_on "bison" => :build
-  depends_on "md5sha1sum" => :build
   depends_on "autoconf" => :build
   depends_on "automake" => :build
+  depends_on "libffi" => :build
+  depends_on "mcpp" => :build
+  depends_on "pkg-config" => :build
 
   def install
-    system "./configure", "--disable-silent-rules",
-                          "--prefix=#{prefix}"
+    system "export "
+    system "./configure", "PKG_CONFIG_PATH=/usr/local/opt/libffi/lib/pkgconfig/",
+           "--disable-silent-rules", "--prefix=#{prefix}"
     system "make", "install"
   end
 
