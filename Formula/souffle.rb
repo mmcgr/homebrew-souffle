a2a29db074054ae3b828434d36e280f6e6963713
mmcgr <mmcgr@users.noreply.github.com>
2019-03-08 11:47:57 +1100

    hardwire some version info

--- Formula/souffle.rb
+++ Formula/souffle.rb
@@ -1,20 +1,25 @@
 class Souffle < Formula
   desc "Translator of declarative Datalog programs into the C++ language."
   homepage "https://github.com/souffle-lang/souffle/wiki"
-  head "https://github.com/souffle-lang/souffle.git"
   url "https://github.com/souffle-lang/souffle/archive/1.5.1.tar.gz"
   sha256 "d620b7f5f67604a0bbfeb7a1f6721c18e5b5b23698758aab2a1a8520aecc6f0b"
+  head "https://github.com/souffle-lang/souffle.git"
 
   depends_on "libtool" => :build
   depends_on "bison" => :build
   depends_on "autoconf" => :build
   depends_on "automake" => :build
   depends_on "libffi" => :build
-  depends_on "mcpp" => :build
+  depends_on "mcpp"
   depends_on "pkg-config" => :build
 
   def install
     system "export", "PKG_CONFIG_PATH=/usr/local/opt/libffi/lib/pkgconfig/"
+    system "git", "init"
+    system "git", "add", "bootstrap"
+    system "git", "commit", "-mbegin"
+    system "git", "tag", "1.5.1"
+    
     system "./bootstrap"
     system "./configure", "--disable-silent-rules",
                           "--prefix=#{prefix}"
